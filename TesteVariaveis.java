8c60dc0f00d1424107b3b94eb1602b07be649f1e
Camila Luiza <78321076+camilaluyza@users.noreply.github.com>
2022-08-04 15:31:32 -0300

    Add files via upload

---
+++ TesteVariaveis.java
@@ -0,0 +1,24 @@
+
+public class TesteVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = 20 -5;
+		
+		System.out.println(idade);
+		
+	}
+	
+	
+
+}
